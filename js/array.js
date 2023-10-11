df2cef19756125d6a305a1d97b62cb98bf4f2405
fantom030983 <ihorkavun1234@gmail.com>
2023-10-11 11:37:04 +0300

    Reboot array

--- js/array.js
+++ js/array.js
@@ -6,10 +6,32 @@
             alert('Отмена');
             
         }
-
+        
         if(arrNumber[i] === '') {
             alert('Ви не вказали число');
-            arr[i] = prompt('Введіть будь-яке число від 1 до 10'); 
+            arr[i] = prompt('Введіть будь-яке число від 1 до 10');
+            
+        }
+        
+        }
+        console.log('Елементи масиву введені User', arrNumber);
 
-        }}
-        console.log(arrNumber)
+        let count = arrNumber.length - 1;
+        let arrNumGrowth = arrNumber
+    
+        for (let i = 0; i < count; i++)
+          for (let j = 0; j < count - i; j++)
+            if (arrNumGrowth[j] > arrNumGrowth[j + 1]) {
+              let max = arrNumGrowth[j];
+              arrNumGrowth[j] = arrNumGrowth[j + 1];
+              arrNumGrowth[j + 1] = max;
+              
+            }
+        
+        console.log('Елементи масиву в порядку зростання', arrNumGrowth);
+
+
+        arrNumber.splice(2, 3);
+        let arrNumDel = [arrNumber];
+        outArrNumber.innerHTML = arrNumDel;
+    
